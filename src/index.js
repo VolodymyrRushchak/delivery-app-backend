11ef562dcb883d4bb91ca6b4bbce12ee45a2cb7e
Volodymyr Rushchak <rushvolod@gmail.com>
2026-04-02 14:34:57 +0300

    Set up express

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: 'It works!' });
+});
+
+const port = process.env.PORT;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
