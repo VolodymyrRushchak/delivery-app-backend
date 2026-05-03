2be46ed7420eba19bdd1037dd0777ce044e800cc
Volodymyr Rushchak <rushvolod@gmail.com>
2026-05-03 14:58:11 +0300

    Add pagination support and do a bit of refactoring

--- src/api/shops.js
+++ src/api/shops.js
@@ -19,34 +19,78 @@ shopsRouter.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
         const shop = await Shop.findById(id);
-        if (!shop) return res.status(404).json({ message: 'The shop was not found.' });
+        if (!shop) return shopNotFound(res);
         res.status(200).json(shop);
     } catch (error) {
-        if (error instanceof mongoose.Error.CastError && error.path === '_id')
-            return res.status(400).json({ message: 'Wrong id format.' });
-        handleServerError(error, res);
+        handleShopError(error, res);
     }
     
 });
 
 shopsRouter.get('/:id/products', async (req, res) => {
     const { id } = req.params;
-    const { categories, sort } = req.query;
+    let { categories, sort, skip, take } = req.query;
     try {
+        if (!(await shopExists(id)))
+            return shopNotFound(res);
         const filter = { shop: id };
-        if (typeof categories === "string" || (Array.isArray(categories) && categories.length))
+        if (validCategories(categories))
             filter.category = { $in: categories };
         let query = Product.find(filter);
+
         if (sort === 'by-price-asc') query.sort({ price: 1 });
         else if (sort === 'by-price-desc') query.sort({ price: -1 });
         else if (sort === 'by-name') query.sort({ name: 1 });
+
+        take = +take;
+        skip = +skip;
+        if (Number.isInteger(take) && Number.isInteger(skip))
+            query.skip(skip).limit(take);
+
         const products = await query.exec();
         res.status(200).json(products);
     } catch (error) {
-        if (error instanceof mongoose.Error.CastError && error.path === 'shop')
-            return res.status(400).json({ message: 'Wrong shopId format.' });
-        handleServerError(error, res);
+        handleShopError(error, res);
     }
 });
+
+shopsRouter.get('/:id/product-count', async (req, res) => {
+    const { id } = req.params;
+    const { categories } = req.query;
+    try {
+        if (!(await shopExists(id)))
+            return shopNotFound(res);
+        const filter = { shop: id };
+        if (validCategories(categories))
+            filter.category = { $in: categories };
+        const count = await Product.countDocuments(filter); 
+        res.status(200).json({ count });
+    } catch (error) {
+        handleShopError(error, res);
+    }
+});
+
+function handleShopError(error, res) {
+    if (error instanceof mongoose.Error.CastError) {
+        if (error.path === '_id') 
+            return res.status(400).json({ message: 'Wrong id format.' });
+        if (error.path === 'shop')
+            return res.status(400).json({ message: 'Wrong shop id format.' });
+    }
+    handleServerError(error, res);
+}
+
+async function shopExists(id) {
+    const shop = await Shop.findById(id);
+    return shop ? true : false;
+}
+
+function shopNotFound(res) {
+    res.status(404).json({ message: 'The shop was not found.' });
+}
+
+function validCategories(categories) {
+    return typeof categories === 'string' || (Array.isArray(categories) && categories.length);
+}
 
 module.exports = shopsRouter;
